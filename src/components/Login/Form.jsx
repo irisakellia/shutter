11b999c7a416c9976a585d3160edb65203fa2cf4
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-06-18 09:29:13 +0200

    [++]

--- src/components/Login/Form.jsx
+++ src/components/Login/Form.jsx
@@ -12,9 +12,11 @@ const Form = () => {
         <div className='text-lg mb-4'>or Sign in with <span className='text-red-600'>Email</span></div>
         <div className='w-full h-px bg-gray-400 mb-4'></div>
       </div>
-      <form className='flex flex-col items-center w-full max-w-xs'>        
+      <form className='flex flex-col items-center w-full max-w-xs'>    
+      <input type='name' placeholder='kellia' id='name' className='w-full p-2 mb-4 border border-gray-300 rounded' />    
         <label htmlFor='Password' className='w-full text-left mb-2 text-red-600'></label>
         <input type='password' placeholder='****' id='password' className='w-full p-2 mb-4 border border-gray-300 rounded' />
+     
       </form>
       <p className=' mb-4 cursor-pointer'>Forgot password?</p>
       <button className=' bg-gradient-to-b from-blue-200 via-red-600 to-red-800 text-white py-2 px-4 rounded mb-4'><Link to={"/home"}>Login</Link></button>
