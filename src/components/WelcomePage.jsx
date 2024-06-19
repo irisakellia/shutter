44a8e037d39e93dd3b2379af606ee484f7488339
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-06-19 13:11:28 +0200

    [++]

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -4,13 +4,21 @@ import { motion } from 'framer-motion';
 const WelcomePage = () => {
   return (
     <motion.div
-      className='text-3xl text-red-600'
+      className='text-3xl text-red-600 '
       initial={{ opacity: 1, y: 50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 3 }}
     >
       Welcome to shutter come view our gallery 
+
+      <p className='text-black text-lg mt-10'>Our photography 
+        <button className='bg-red-600 p-2 py-2 rounded-xl text-white text-sm ml-10 '>
+          View Gallery
+        </button>
+      </p>
     </motion.div>
+
+    
 
     
   );
