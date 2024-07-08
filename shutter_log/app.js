42d1bb59b0604cb66230a435efe0e6cba2bc6dcd
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-08 20:53:10 +0200

    [added]

--- shutter_log/app.js
+++ shutter_log/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const port = 4001 ;
 const cors = require('cors')
 const signupRoute = require('./routes/signup')
+const loginRoute = require('./routes/login')
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,7 @@ app.use(cors());
 
 dbConnection();
 app.use('/user',signupRoute)
+app.use('/auth',loginRoute)
 
 app.listen(port,()=>{
     console.log(`the server is running on port ${port}`)
