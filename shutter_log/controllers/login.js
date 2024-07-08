30d33399143a75f0741a1685473b0f0b88da8fb0
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-08 20:47:22 +0200

    [added]

--- shutter_log/controllers/login.js
+++ shutter_log/controllers/login.js
@@ -1,9 +1,16 @@
-async function login(){
+const Service = require('../service/login')
+
+async function login(req,res){
 try {
+    const{email,password} = req.body ;
+
+    const token = Service.login(email,password);
+
+    res.json({token:token});
    
     
 
-    
+
 } catch (error) {
     res.status(400).json({success:false,message:"invalid credentials"})
 }
--- shutter_log/service/login.js
+++ shutter_log/service/login.js
@@ -1,1 +1,26 @@
-const bcrypt = require('')
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+const {generateToken} = require('../utils/jwtUtils')
+
+async function login(email,password){
+    try {
+        const existingUser = User.findOne({email});
+
+        if(!existingUser){
+            console.error("user does not exist in my database")
+        }
+const isPasswordValid = bcrypt.compare(password , existingUser.password);
+if(!isPasswordValid){
+    console.error("the password is invalid")
+
+}
+const token = generateToken(existingUser);
+return token ;
+
+    } catch (error) {
+        res.status(400).json({success:false,message:"login failed"})
+    }
+
+}
+
+module.exports = {login}
