2479dd63e3003cc799c5159dcce452914f2330fb
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-08 16:15:49 +0200

    [added]

--- shuter/src/components/signup/Signup.jsx
+++ shuter/src/components/signup/Signup.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Form = () => {
+  
+
   return (
     <div className='flex flex-col items-center p-4'>
       <p className='text-2xl mb-4'>Hello!</p>
@@ -12,15 +14,24 @@ const Form = () => {
         <div className='text-lg mb-4'>or Sign in with <span className='text-red-600'>Email</span></div>
         <div className='w-full h-px bg-gray-400 mb-4'></div>
       </div>
-      <form className='flex flex-col items-center w-full max-w-xs'>
+      <form className='flex flex-col items-center w-full max-w-xs' onSubmit={handleSubmit}>
         <label htmlFor='name' className='w-full text-left mb-2 text-red-600'></label>
-        <input type='email' placeholder='kellia' id='email' className='w-full p-2 mb-4 border border-gray-300 rounded' />
+        <input type='text' placeholder='kellia' id='name' className='w-full p-2 mb-4 border border-gray-300 rounded'
+        value={data.name}
+        onChange={handleInputChange}
+        />
 
         <label htmlFor='Email' className='w-full text-left mb-2 text-red-600'></label>
-        <input type='email' placeholder='irisakellia@gmail.com' id='email' className='w-full p-2 mb-4 border border-gray-300 rounded' />
+        <input type='email' placeholder='irisakellia@gmail.com' id='email' className='w-full p-2 mb-4 border border-gray-300 rounded'
+        value={data.email}
+        onChange={handleInputChange}
+        />
         
         <label htmlFor='Password' className='w-full text-left mb-2 text-red-600'></label>
-        <input type='password' placeholder='****' id='password' className='w-full p-2 mb-4 border border-gray-300 rounded' />
+        <input type='password' placeholder='****' id='password' className='w-full p-2 mb-4 border border-gray-300 rounded' 
+        value={data.password}
+        onChange={handleInputChange}
+        />
       </form>
       <p className=' mb-4 cursor-pointer'>Forgot password?</p>
       <button className=' bg-gradient-to-b from-blue-200 via-red-600 to-red-800 text-white py-2 px-4 rounded mb-4'><Link to={"/login"}>SignUp</Link></button>
