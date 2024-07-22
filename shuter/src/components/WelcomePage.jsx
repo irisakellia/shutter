0eaa656d70a7cd10f0a3c620f71fe2de1b11dcb2
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-22 18:20:31 +0200

    [adding]

--- shuter/src/components/WelcomePage.jsx
+++ shuter/src/components/WelcomePage.jsx
@@ -11,17 +11,11 @@ const WelcomePage = () => {
 
   return (
     <div>
-      <p>Welcome to Shutter! Come view our gallery.</p>
-
-      <p className='text-black text-lg mt-10'>
-        Our photography
-        <button className='bg-red-600 p-2 py-2 rounded-xl text-white text-sm ml-10'>
-          View Gallery
-        </button>
+    
         <button className='float-right' onClick={handleLogout}>
           Log out
         </button>
-      </p>
+      
     </div>
   );
 };
