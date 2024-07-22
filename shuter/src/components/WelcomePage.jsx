78229d832c58e0bf374cee7f970e3ae9babe465a
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-22 18:57:27 +0200

    [adding]

--- shuter/src/components/WelcomePage.jsx
+++ shuter/src/components/WelcomePage.jsx
@@ -47,11 +47,11 @@ const WelcomePage = () => {
       <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <div className="bg-white p-4 shadow rounded-lg">
           <div className="text-gray-600 font-serif">TOTAL Images</div>
-          <div className="text-3xl font-bold">95</div>
-          <div className="text-sm text-gray-500">25% Improvement</div>
+          <div className="text-3xl font-bold">95<sup>+</sup></div>
+          
         </div>
         <div className="bg-white p-4 shadow rounded-lg">
-          <div className="text-gray-600">NEW LEADS</div>
+          <div className="text-gray-600">NEW ALBUMS</div>
           <div className="text-3xl font-bold">21</div>
           <div className="text-sm text-gray-500">80% Daily Goal</div>
           <div className="text-sm text-gray-500">72 This week</div>
